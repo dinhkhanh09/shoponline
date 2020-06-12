b195700c828be5f81833102667b504812802529f
dinhkhanh09 <56311851+dinhkhanh09@users.noreply.github.com>
2020-06-12 13:32:19 +0700

    first commit

---
+++ ShopOnline/Controllers/TempController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopOnline.Models;
+
+
+namespace ShopOnline.Controllers
+{
+    public class TempController : Controller
+    {
+        WebBanhangEntities2 _db = new WebBanhangEntities2();
+        // GET: TempShopOnlineEntities1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getMenu()
+        {
+            var v = from t in _db.menus
+                    where t.hide == true
+                    orderby t.order ascending
+                    select t;
+            return PartialView(v.ToList());
+        }
+
+    }
+
+}
